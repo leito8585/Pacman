27d116e58f70e96a4552ef4cb71150199342f2ce
Jakub Piorecki <Jakub.Piorecki@takko.de>
2022-05-17 09:29:26 +0200

    Kartengrenzen generieren

--- index.js
+++ index.js
@@ -3,3 +3,49 @@ const context = canvas.getContext("2d");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+class Grenze {
+  static width = 40;
+  static height = 40;
+  constructor({ position }) {
+    this.position = position;
+    this.width = 40;
+    this.height = 40;
+  }
+
+  draw() {
+    context.fillStyle = "blue";
+    context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const grenzen = [];
+
+const map = [
+  ["-", "-", "-", "-", "-", "-", "-"],
+  ["-", " ", " ", " ", " ", " ", "-"],
+  ["-", " ", "-", " ", "-", " ", "-"],
+  ["-", " ", " ", " ", " ", " ", "-"],
+  ["-", "-", "-", "-", "-", "-", "-"],
+];
+
+map.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    switch (symbol) {
+      case "-":
+        grenzen.push(
+          new Grenze({
+            position: {
+              x: Grenze.width * j,
+              y: Grenze.height * i,
+            },
+          })
+        );
+        break;
+    }
+  });
+});
+
+grenzen.forEach((grenze) => {
+  grenze.draw();
+});
